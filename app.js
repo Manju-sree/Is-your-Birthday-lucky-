b2191fd29e8ec7b6e1e1a8cbe807224e81751b45
Manju-sree <manjushreek9@gmail.com>
2021-08-18 21:41:35 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const dateOfBirth = document.querySelector('#date-of-birth');
+const luckyNumber = document.querySelector('#lucky-number');
+const buttonCheckNumber = document.querySelector('#check-number');
+const outputBox = document.querySelector('#output-box');
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (let index = 0; index < dob.length; index++) {
+        sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+}
+
+function compareValues(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        outputBox.innerText = "Your Birthday is lucky 🤩";
+    } else {
+        outputBox.innerText = 'Your Birthday is not lucky 😑';
+    }
+
+}
+
+function checkDateOfBirthIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    if (sum && dob) {
+        compareValues(sum, luckyNumber.value)
+    } else {
+        outputBox.innerText = "Please enter both the fields 😈";
+    }
+
+}
+buttonCheckNumber.addEventListener("click", checkDateOfBirthIsLucky);
